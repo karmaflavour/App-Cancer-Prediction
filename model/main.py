cfc7dce19fb2acbc5e280940edd384b847845073
Aidan Duong <aidan.duong@gmx.de>
2024-01-16 13:54:05 +0100

    First commit

---
+++ model/main.py
@@ -0,0 +1,16 @@
+
+
+
+def main():
+    # Create the model
+    model = create_model()
+
+    # Train the model
+    train(model)
+
+    # Evaluate the model
+    evalute(model)
+
+if __name__ == "__main__":
+    main()
+    
