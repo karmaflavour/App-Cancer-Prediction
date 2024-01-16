8d5eff15ade47c145b5816622297906f1788926a
Aidan Duong <aidan.duong@gmx.de>
2024-01-16 19:53:41 +0100

    Setup Streamlit, dump model and scaler

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+def main():
+    st.set_page_config(
+        page_title="Breast Cancer Predictor",
+        page_icon=":female-doctor:",
+        layout="wide",
+        initial_sidebar_state="expanded"
+        )
+    
+    st.write("Hello world")
+
+
+
+if __name__ == "__main__":
+    main()
--- model/main.py
+++ model/main.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+import pickle
 
 
 def create_model(data):
@@ -31,12 +32,6 @@ def create_model(data):
 
 
 
-
-
-
-
-
-
 def get_clean_data():
     data = pd.read_csv("data/data.csv")
     
@@ -52,6 +47,13 @@ def main():
     data = get_clean_data()
     
     model, scaler = create_model(data)
+
+    with open("model/model.pkl", "wb") as f:
+        pickle.dump(model, f)
+
+    with open("model/scaler.pkl", "wb") as f:
+        pickle.dump(scaler, f)
+
 
 
 if __name__ == "__main__":
